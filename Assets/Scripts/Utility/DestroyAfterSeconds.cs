910432a56928ab44acd3cd28efb000fbe193093d
jzenzinger <juris.zen@seznam.cz>
2023-07-12 07:23:36 +0200

    Git problem

---
+++ Assets/Scripts/Utility/DestroyAfterSeconds.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    public float seconds;
+
+    void Start()
+    {
+        Destroy(gameObject, seconds);
+    }
+
+}
